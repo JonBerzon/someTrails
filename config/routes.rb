076bf24d111034c1fcdc40d08eeedd35c1e0bf94
Jonathan Berzon <jonathanlberzon@gmail.com>
2021-10-28 15:03:22 -0400

    back end auth completed

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: {format: :json} do 
     resources :users, only: [:create, :show, :update]
+    resource :session, only: [:create, :destroy]
   end
 end
