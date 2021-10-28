54aa590a55568570a9579697157bb0d4c56af6d6
Jonathan Berzon <jonathanlberzon@gmail.com>
2021-10-28 15:41:57 -0400

    added session actions and reducers

---
+++ frontend/components/app.jsx
@@ -0,0 +1,11 @@
+import { Route } from "react-router-dom"
+import React from "react"
+
+
+const App = () => (
+    <div>
+        "React is working :)"
+    </div>
+)
+
+export default App;
---
+++ frontend/reducers/root_reducer.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+
+const rootReducer = combineReducers({
+    users: {hi:"hi"}
+})
+
+export default rootReducer;
