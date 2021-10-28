186f39b2ce2bb33951eb282ad96b7409faff6d04
Jonathan Berzon <jonathanlberzon@gmail.com>
2021-10-28 14:34:02 -0400

    user_controller added

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,38 @@
+class Api::UsersController < ApplicationController
+    def show
+        @user = User.find(params[:id])
+        render :show
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            login!(@user)
+            render :show 
+        else
+            render json: @user.errors.full_messages, status: 401
+        end
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user == current_user
+            if @user.update(user_params)
+                render :show
+            else
+                @user.errors.full_messages, status: 401
+            end
+        else
+            render json: ['Could not locate user'], status: 400
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:email, :password, :bio, :first_name, :last_name)
+    end
+
+
+
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,43 @@
+class User < ApplicationRecord
+    #figvaper
+
+    attr_reader :password
+
+    validates :email, presence: true, uniqueness: true
+    validates :password_digest, :session_token, presence: true 
+    validates :password, length: { minimum: 6 }, allow_nil: true
+    # validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } #validates email format
+    after_initialize :ensure_session_token
+
+    def self.find_by_credentials(email, password)
+        user = User.find_by(email: email)
+        return nil if user.nil?
+        user.is_password?(password) ? user : nil
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def self.generate_session_token
+        SecureRandom.urlsafe_base64(16)
+    end
+
+    def password=(password)
+        @password = password 
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def ensure_session_token
+        self.session_token ||= self.generate_session_token
+    end
+
+    def reset_session_token!
+        self.session_token = self.generate_session_token
+        self.save!
+        self.session_token
+    end
+
+
+
+end
