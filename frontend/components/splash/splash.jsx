1747da7bca46ca92f3447314b19028b1a7fb7dcf
Jonathan Berzon <jonathanlberzon@gmail.com>
2021-10-29 17:59:32 -0400

    splash stats are done with appropiate media queries

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -9,7 +9,28 @@ class Splash extends React.Component{
 
     render(){
         return(
-            <SearchbarContainer />
+            <div>
+                <div>
+                    <SearchbarContainer />
+                </div>
+                <div className="splash-stats-div">
+                    <div className="splash-stats">
+                        <h1>200k+</h1>
+                        <h2>curated trails</h2>
+                        <p>Discover unexpected gems, even in your own backyard.</p>
+                    </div>
+                    <div className="splash-stats">
+                        <h1>25 mil</h1>
+                        <h2>fellow explorers</h2>
+                        <p>Share your adventures and learn from our global community.</p>
+                    </div>
+                    <div className="splash-stats">
+                        <h1>214 mil</h1>
+                        <h2>logged miles</h2>
+                        <p>Navigate your way and keep a record of all your travels.</p>
+                    </div>
+                </div>
+            </div>
         )
     }
 
