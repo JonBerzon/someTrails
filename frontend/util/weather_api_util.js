b6fa3c19260df586f3c534ca594a469818e71349
Jonathan Berzon <jonathanlberzon@gmail.com>
2021-11-07 11:51:25 -0500

    weather updated to https

--- frontend/util/weather_api_util.js
+++ frontend/util/weather_api_util.js
@@ -2,7 +2,7 @@ export const fetchWeather = (coord) => {
     return $.ajax({
         method: "GET",
         // url: `http://api.weatherapi.com/v1/forecast.json?key=1a2d8bae081c402ba5c175223210511&q=${coord.lat},${coord.long}&days=5&aqi=no&alerts=no`
-        url: `http://api.weatherapi.com/v1/forecast.json?key=1a2d8bae081c402ba5c175223210511&q=${coord.lat},${coord.long}&days=3&aqi=no&alerts=no`
+        url: `https://api.weatherapi.com/v1/forecast.json?key=1a2d8bae081c402ba5c175223210511&q=${coord.lat},${coord.long}&days=3&aqi=no&alerts=no`
 
     })
 }
