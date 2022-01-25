684cac51689dad5a1eaa86c0d087280912444dfe
Jonathan Berzon <jonathanlberzon@gmail.com>
2022-01-25 10:52:15 -0500

    added copyright

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -13,11 +13,15 @@ class Footer extends React.Component{
                         <img className="footer-logo" src={window.logo} alt="" />
                         <p className="footer-title">SomeTrails</p>
                     </div>
+                    
                     <div className="right-footer">
                         <a href="https://github.com/JonBerzon/someTrails" target="_blank"><img className="git-logo" src={window.git} /></a>
                         <a href="https://www.linkedin.com/feed/" target="_blank"><img className="linked-logo" src={window.linked} /></a>
                         <a href="https://angel.co/u/jonathan-berzon" target="_blank"><img className="angel-logo" src={window.angel} /></a>
                     </div>
+                </div>
+                <div className="copyright">
+                    &#169; Jonathan Berzon 2021
                 </div>
             </footer>
             
